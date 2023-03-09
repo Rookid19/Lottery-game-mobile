c4b4443849e38c45e328cad600767bcabe089c67
rookid19 <>
2023-03-09 15:58:09 +0000

    draw balls

--- src/components/DrawBalls.jsx
+++ src/components/DrawBalls.jsx
@@ -1,13 +1,18 @@
-import React from 'react'
-import { MdArrowDropDown } from 'react-icons/md'
-import "../css/DrawBalls.css"
+import React from "react";
+import { MdArrowDropDown } from "react-icons/md";
+import "../css/DrawBalls.css";
+import ball from "../images/reel.png";
 function DrawBalls() {
+  const balls = [1, 2, 3, 4];
   return (
-    <div className='draw_balls_wrapper'>
+    <div className="draw_balls_wrapper">
       <span>001179</span>
+      {balls.map((number) => (
+        <img key={number} src={ball} id="image_ball" alt="" />
+      ))}
       <MdArrowDropDown size={20} />
     </div>
-  )
+  );
 }
 
-export default DrawBalls
+export default DrawBalls;
