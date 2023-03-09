eeaebd3009e4d87f11adba8842c722a8cc0e082a
rookid19 <>
2023-03-09 16:44:20 +0000

    tab.css

--- src/components/Tabs.jsx
+++ src/components/Tabs.jsx
@@ -4,11 +4,25 @@ import "../css/Tabs.css";
 
 function Tabs() {
   const tabs_buttons = ["2 Sides", "B", "C", "D"];
+  const rows = [
+    "First 3 Straight (Joint)",
+    "First 3 Straight (Manual)",
+    "All 5 Straight(Combo)",
+  ];
+
   return (
-    <div className="tab_wrapper">
-      <div className="tab_container">
-        {tabs_buttons.map((letter) => (
-          <button className="tab_button">{letter}</button>
+    <div className="tab">
+      <div className="tab_wrapper">
+        <div className="tab_container">
+          {tabs_buttons.map((letter) => (
+            <button className="tab_button">{letter}</button>
+          ))}
+          <MdArrowDropDown size={20} />
+        </div>
+      </div>
+      <div className="tab2_container">
+        {rows.map((letter) => (
+          <button className="tab_button2">{letter}</button>
         ))}
         <MdArrowDropDown size={20} />
       </div>
